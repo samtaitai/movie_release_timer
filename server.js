f408911a21b777f64ed9026a118680c42c9b6948
Soyon <samtaitai@gmail.com>
2022-08-16 09:54:10 -0400

    fix for deploy

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/test', function(req, res){
     res.sendFile( __dirname + "/" + "test.html" );
 });
 
-var server = app.listen(8081, function(){
+var server = app.listen(process.env.PORT || 8081, function(){
     var host = server.address().address
     var port = server.address().port
 
